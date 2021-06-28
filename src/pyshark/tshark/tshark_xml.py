c490a09ad16c3e383ed5cd9310c7913065836b8f
Yablargo <matthew.galligan@gmail.com>
2021-06-28 10:59:40 -0400

    Applied Main repo MR https://github.com/KimiNewt/pyshark/pull/479/files

--- src/pyshark/tshark/tshark_xml.py
+++ src/pyshark/tshark/tshark_xml.py
@@ -11,6 +11,10 @@ def psml_structure_from_xml(psml_structure):
         psml_structure = lxml.objectify.fromstring(psml_structure)
     return psml_structure.findall('section')
 
+DEL_BAD_XML_CHARS = {bad_char: None for bad_char in range(0x00, 0x20) if not bad_char in (0x09, 0x0a, 0x0d)}
+DEL_BAD_XML_CHARS.update({bad_char: None for bad_char in range(0xd800, 0xe000)})
+DEL_BAD_XML_CHARS.update({bad_char: None for bad_char in range(0xfffe, 0x10000)})
+
 
 def packet_from_xml_packet(xml_pkt, psml_structure=None):
     """
@@ -23,6 +27,7 @@ def packet_from_xml_packet(xml_pkt, psml_structure=None):
     """
     if not isinstance(xml_pkt, lxml.objectify.ObjectifiedElement):
         parser = lxml.objectify.makeparser(huge_tree=True, recover=True)
+        xml_pkt = xml_pkt.decode(errors='ignore').translate(DEL_BAD_XML_CHARS)
         xml_pkt = lxml.objectify.fromstring(xml_pkt, parser)
     if psml_structure:
         return _packet_from_psml_packet(xml_pkt, psml_structure)
